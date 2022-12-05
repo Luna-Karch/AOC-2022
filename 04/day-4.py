e45ab519e8612950e5cb99d08692edcba0583f05
Luna-Karch <luna.karch05@gmail.com>
2022-12-04 20:10:10 -0500

    Day 4 Part 2

--- 04/day-4.py
+++ 04/day-4.py
@@ -19,4 +19,23 @@ def part_1():
     return total_overlaps
 
 
+def part_2():
+    with open("input.txt", "r", encoding="utf-8") as f:
+        data = [i.strip() for i in f.readlines()]
+
+    total_overlaps = 0
+
+    for line in data:
+        range_one, range_two = line.split(",")
+        range_one_start, range_one_end = map(int, range_one.split("-"))
+        range_two_start, range_two_end = map(int, range_two.split("-"))
+        the_range_one = list(range(range_one_start, range_one_end + 1))
+        the_range_two = list(range(range_two_start, range_two_end + 1))
+        if [item for item in the_range_one if item in the_range_two]:
+            total_overlaps += 1
+
+    return total_overlaps
+
+
 print(part_1())
+print(part_2())
